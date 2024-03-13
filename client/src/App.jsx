734fe8572ee5f9cb2d6a42899cd46a5ad9fef2f7
aryantiw <aryantiwari543@gmail.com>
2024-03-13 12:11:35 +0530

    add: pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route  } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+
+
 
 const App = () => {
   return (
-    <div>
-      
-      <h1 className="text-red-500">hello</h1>
-      
-    </div>
+    <BrowserRouter> 
+    
+    <Header/>
+
+    <Routes>
+
+    <Route path="/" element={Home}></Route>
+    <Route path="/sign-in" element={<SignIn/>}></Route>
+    <Route path="/sign-up" element={<SignUp/>}></Route>
+    <Route path="/about" element={<About/>}></Route>
+    <Route path="/" element={<Profile/>}></Route>
+
+    </Routes>
+    </BrowserRouter>
   )
 }
 
