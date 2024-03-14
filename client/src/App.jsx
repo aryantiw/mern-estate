6543495c52ea606387c841cd941e00ccfe0ee1b5
aryantiw <aryantiwari543@gmail.com>
2024-03-14 23:34:37 +0530

    add: navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,11 +16,11 @@ const App = () => {
 
     <Routes>
 
-    <Route path="/" element={Home}></Route>
+    <Route path="/" element={<Home/>}></Route>
     <Route path="/sign-in" element={<SignIn/>}></Route>
     <Route path="/sign-up" element={<SignUp/>}></Route>
     <Route path="/about" element={<About/>}></Route>
-    <Route path="/" element={<Profile/>}></Route>
+    <Route path="/profile" element={<Profile/>}></Route>
 
     </Routes>
     </BrowserRouter>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {FaSearch} from 'react-icons/fa'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='bg-slate-200 shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+          <span className="text-slate-500">MERN</span>
+          <span className="text-slate-700">Estate</span>
+        </h1>
+      
+
+      <form className='bg-slate-100 p-2 rounded flex items-center'>
+        <input type="text" placeholder='Search' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+        <FaSearch className='text-slate-600'/>
+
+      </form>
+      <ul className='flex gap-4'>
+        <li className='hidden sm:inline text-slate-700 hover:underline '>Home</li>
+        <li className='hidden sm:inline text-slate-700 hover:underline '>About</li>
+        <li className='sm:inline text-slate-700 hover:underline '>SignIn</li>
+      </ul>
+
+      </div>
+    </header>
   )
 }
 
