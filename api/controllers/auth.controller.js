157fd7b10b96476b629ace868130c8074a50b8d3
aryantiw <aryantiwari543@gmail.com>
2024-03-20 23:39:09 +0530

    add singup api

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,25 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import { errorHandler } from "../utils/error.js";
+
+export const signup = async(req, res, next) =>{
+
+    const { username, email, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password,10);
+
+    const newUser = new  User({ username ,email  ,password: hashedPassword});
+
+
+    try{
+        await newUser.save();
+        res.status(201).json("User Created Successfully")
+    }
+    catch(error){
+        next(error);
+
+    }
+   
+
+
+
+};
--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from './routes/auth.route.js'
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() =>{
+
+
+mongoose.connect(process.env.MONGO).then(() => {
     console.log("Mongodb is connected");
 }).catch((err) => {
     console.log("Error: " + err);
@@ -14,6 +18,21 @@ mongoose.connect(process.env.MONGO).then(() =>{
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Port is running on 3000");
 })
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
